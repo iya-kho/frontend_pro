e91e3473325fccd708f53017051f878aec68889e
Iya Shchur <iya.schur@gmail.com>
2022-10-28 22:31:40 +0300

    hw_3_task_2

---
+++ hw_3/js/script_2.js
@@ -0,0 +1,18 @@
+let operation = prompt('What operation do you want to perform?', 'add, sub, mult, div')
+let number1 = Number(prompt('Please enter the first number', '1'))
+let number2 = Number(prompt('Please enter the second number', '2'))
+
+if (operation === 'add') {
+    message = `${number1} + ${number2} = ${number1 + number2}`;
+} else if (operation === 'sub') {
+    message = `${number1} - ${number2} = ${number1 - number2}`;
+} else if (operation === 'mult') {
+    message = `${number1} * ${number2} = ${number1 * number2}`;
+} else if (operation === 'div') {
+    message = `${number1} / ${number2} = ${number1 / number2}`;
+} else {
+    message = 'Wrong choice. Please choose betweem add, sub, mult or div';
+}
+
+
+alert(message)
