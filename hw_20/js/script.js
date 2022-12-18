e117d94d0a98b1892da55179168717675c1e7cf6
Iya Shchur <iya.schur@gmail.com>
2022-12-18 21:22:44 +0200

    hw_20_this_chain

---
+++ hw_20/js/script.js
@@ -0,0 +1,17 @@
+let ladder = {
+    step: 0,
+    up: function () {
+        this.step++;
+        return this;
+    },
+    down: function () {
+        this.step--;
+        return this;
+    },
+    showStep: function () { 
+        alert(this.step);
+    }
+};
+
+
+ladder.up().up().down().down().down().showStep(); // 1
