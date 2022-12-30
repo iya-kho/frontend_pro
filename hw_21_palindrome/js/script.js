94eab94f0c3002e7b65ac334863b4ac2575649b0
Iya Shchur <iya.schur@gmail.com>
2022-12-30 15:03:16 +0200

    hw_2 versions added

--- hw_21_palindrome/js/script.js
+++ hw_21_palindrome/js/script.js
@@ -4,25 +4,15 @@
 // Remove One - якщо можна видалити одну букву(яка є "зайвою") і строка стане паліндромом
 // Not Possible - якщо жодна з умов не спрацювала
 
-function singleCharacterPalindrome(str) {
+// Version 1: recursion
+
+function palindromeRecursion(str) {
     const toArr = str.split('');
     let message = '';
 
-    function minusLetter(arr, i = 0) {
-        let arrCopy = [...arr];
-        arrCopy.splice(i, 1);
-        if (i >= arrCopy.length) {
-            return false;
-        } else if (arrCopy.toString() === arrCopy.reverse().toString()) {
-            return true;
-        } else {
-            return minusLetter(arr, i += 1);
-        }
-    }
-    
     if (toArr.toString() === toArr.reverse().toString()) {
         message = 'OK';
-    } else if (minusLetter(toArr)) {
+    } else if (minusLetterRecursion(toArr)) {
         message = 'Remove One';
     } else {
         message = 'Not Possible';
@@ -32,9 +22,98 @@ function singleCharacterPalindrome(str) {
 
 }
 
-console.log(singleCharacterPalindrome("abba")); // "OK"
-console.log(singleCharacterPalindrome("abbaa")); // "Remove One"
-console.log(singleCharacterPalindrome("abbaab")); // "Not Possible"
-console.log(singleCharacterPalindrome("madmam")); // "Remove One"
-console.log(singleCharacterPalindrome("raydarm")); // "Not Possible"
-console.log(singleCharacterPalindrome("hannah")); // "OK"
+function minusLetterRecursion(arr, i = 0) {
+    let arrCopy = [...arr];
+    arrCopy.splice(i, 1);
+    if (i >= arrCopy.length) {
+        return false;
+    } else if (arrCopy.toString() === arrCopy.reverse().toString()) {
+        return true;
+    } else {
+        return minusLetterRecursion(arr, i += 1);
+    }
+}
+
+    
+
+console.log(palindromeRecursion("abba")); // "OK"
+console.log(palindromeRecursion("abbaa")); // "Remove One"
+console.log(palindromeRecursion("abbaab")); // "Not Possible"
+console.log(palindromeRecursion("madmam")); // "Remove One"
+console.log(palindromeRecursion("raydarm")); // "Not Possible"
+console.log(palindromeRecursion("hannah")); // "OK"
+console.log('----------------------------------------');
+
+// Version 2: for loop
+
+function palindromeLoop(str) {
+    const toArr = str.split('');
+    let message = '';
+    let isPalMinusChar = false;
+
+    for (let i in toArr) {
+        let toArrCopy = [...toArr];
+        toArrCopy.splice(i, 1);
+        if (toArrCopy.toString() === toArrCopy.reverse().toString()) {
+            isPalMinusChar = true;
+            break;
+        }
+    }
+
+    if (toArr.toString() === toArr.reverse().toString()) {
+        message = 'OK';
+    } else if (isPalMinusChar) {
+        message = 'Remove One';
+    } else {
+        message = 'Not Possible';
+    }
+
+    return message;
+
+}
+
+console.log(palindromeLoop("abba")); // "OK"
+console.log(palindromeLoop("abbaa")); // "Remove One"
+console.log(palindromeLoop("abbaab")); // "Not Possible"
+console.log(palindromeLoop("madmam")); // "Remove One"
+console.log(palindromeLoop("raydarm")); // "Not Possible"
+console.log(palindromeLoop("hannah")); // "OK"
+console.log('----------------------------------------');
+
+// Version 3: Array.prototype.every()
+
+function palindromeEvery(str) {
+    const toArr = str.split('');
+    let message = '';
+
+    if (toArr.toString() === toArr.reverse().toString()) {
+        message = 'OK';
+    } else if (!toArr.every(isNotPalMinusCharEvery)) {
+        message = 'Remove One';
+    } else {
+        message = 'Not Possible';
+    }
+
+    return message;
+
+}
+
+
+function isNotPalMinusCharEvery(element, index, arr) {
+    let arrCopy = [...arr];
+    arrCopy.splice(index, 1);
+    if (arrCopy.toString() === arrCopy.reverse().toString()) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+console.log(palindromeEvery("abba")); // "OK"
+console.log(palindromeEvery("abbaa")); // "Remove One"
+console.log(palindromeEvery("abbaab")); // "Not Possible"
+console.log(palindromeEvery("madmam")); // "Remove One"
+console.log(palindromeEvery("raydarm")); // "Not Possible"
+console.log(palindromeEvery("hannah")); // "OK"
+
+
