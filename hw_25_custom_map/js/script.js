17bf648cb440cc81ecd66192bf4dd3eb25b76879
Iya Shchur <iya.schur@gmail.com>
2023-01-30 12:30:53 +0200

    custom_map_reviewed

--- hw_25_custom_map/js/script.js
+++ hw_25_custom_map/js/script.js
@@ -11,14 +11,14 @@ console.log(`JS default map: ${resultDefault}`);
 
 // Custom map
 
-function customMap (arr, fn) {
+function customMap(array, callback) {
     let result = [];
-    for (let i of arr) {
-        let element = fn(i);
-        result.push(element);
+    for (let index = 0; index < array.length; index++) {
+        let newElement = callback(array[index], index, array);
+        result.push(newElement);
     }
+
     return result;
 }
 
 console.log(`Custom map: ${customMap(myArray, myFunc)}`);
-
