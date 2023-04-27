68b116ccdc1e6cc484bf6b7770b6951e0fe7eae4
Iya Shchur <iya.schur@gmail.com>
2023-04-27 23:20:50 +0300

    hw_37

---
+++ hw_37_react_form/src/App.js
@@ -0,0 +1,13 @@
+import { ValidationSchemaExample } from "./components/FormikForm.jsx";
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ValidationSchemaExample />
+    </div>
+  );
+}
+
+export default App;
